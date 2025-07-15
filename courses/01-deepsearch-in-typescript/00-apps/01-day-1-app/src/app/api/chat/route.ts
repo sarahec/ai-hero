ae2a53acbf60b22d3497e968f291a6e4e9d05517
Sarah Clark <seclark@nextquestion.net>
2025-07-15 15:33:23 -0700

    Day 1: Create a naive agent with Serper

--- courses/01-deepsearch-in-typescript/00-apps/01-day-1-app/src/app/api/chat/route.ts
+++ courses/01-deepsearch-in-typescript/00-apps/01-day-1-app/src/app/api/chat/route.ts
@@ -1,9 +1,8 @@
 import type { Message } from "ai";
-import {
-  streamText,
-  createDataStreamResponse,
-} from "ai";
+import { createDataStreamResponse, streamText } from "ai";
+import { z } from "zod";
 import { model } from "~/lib/ai";
+import { searchSerper } from "~/serper";
 import { auth } from "~/server/auth";
 
 export const maxDuration = 60;
@@ -14,7 +13,7 @@ export async function POST(request: Request) {
   if (!session) {
     return new Response("Unauthorized", { status: 401 });
   }
-  
+
   const body = (await request.json()) as {
     messages: Array<Message>;
   };
@@ -23,9 +22,35 @@ export async function POST(request: Request) {
     execute: async (dataStream) => {
       const { messages } = body;
 
-      const result = streamText({
+      const result = await streamText({
         model,
         messages,
+        system: `
+          You are a helpful assistant that can search the web. 
+          1. Please use the search web tool to answer the user's questions. 
+          Cite your sources whenever possible
+          and use inline markdown links like [title](url).
+        `,
+        tools: {
+          searchWeb: {
+            parameters: z.object({
+              query: z.string().describe("The query to search the web for"),
+            }),
+            execute: async ({ query }, { abortSignal }) => {
+              const results = await searchSerper(
+                { q: query, num: 10 },
+                abortSignal,
+              );
+
+              return results.organic.map((result) => ({
+                title: result.title,
+                link: result.link,
+                snippet: result.snippet,
+              }));
+            },
+          },
+        },
+        maxSteps: 10,
       });
 
       result.mergeIntoDataStream(dataStream);
